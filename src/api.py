5577f03cc966ab8a370524a3b1ee4b3f90c25720
hyprd <soulitch7@gmail.com>
2024-01-11 14:59:46 +1300

    Calculates total of single sets

--- src/api.py
+++ src/api.py
@@ -8,20 +8,35 @@ class ItemEncoder(JSONEncoder):
         return o.__dict__
 
 class Item():
-    def __init__(self, name, price_chaos, price_divines):
+    def __init__(self, name, price_chaos):
         self.name = name
         self.price_chaos = price_chaos
-        self.price_divines = price_divines
 
 endpoint_scarabs = 'https://poe.ninja/api/data/itemoverview?league=Affliction&type=Scarab'
 endpoint_compasses = 'https://raw.githubusercontent.com/The-Forbidden-Trove/tft-data-prices/master/lsc/bulk-compasses.json'
 
 Pattern = Enum('Pattern', ['NINJA', 'TFT'])
+ResourceType = Enum('ResourceType', ['SCARAB', 'COMPASS'])
 
 def fetch_resources():
     fetch_resource(endpoint_scarabs, "data_scarabs.json", Pattern.NINJA)
     fetch_resource(endpoint_compasses, "data_compasses.json", Pattern.TFT)
 
+def fetch_resource_names(resource):
+    file = ''
+    values = {}
+    match(resource):
+        case ResourceType.SCARAB: 
+            file = 'data_scarabs.json'
+        case ResourceType.COMPASS:
+            file = 'data_compasses.json'
+    with open(file, 'r') as file:
+        f = json.load(file)
+        for key in f:
+            values.update({key['name']: key['price_chaos']})
+    file.close()
+    return values
+        
 def fetch_resource(endpoint, output_file, pattern):
     if not exists(output_file):
         data = requests.get(endpoint).json()
@@ -29,11 +44,11 @@ def fetch_resource(endpoint, output_file, pattern):
         match(pattern):
             case Pattern.NINJA:
                 for key in data['lines']:
-                    item = Item(key['name'], math.ceil(key['chaosValue']), key['divineValue'])
+                    item = Item(key['name'], math.ceil(key['chaosValue']))
                     items.append(item)
             case Pattern.TFT:
                 for key in data['data']:
-                    item = Item(key['name'], key['chaos'], key['divine'])
+                    item = Item(key['name'], key['chaos'])
                     items.append(item)
         serialized = json.dumps(items, cls=ItemEncoder, indent=4)
         with open(output_file, 'w') as file:
--- src/ui.py
+++ src/ui.py
@@ -1,57 +1,77 @@
 import tkinter.ttk as ttk
+import tkinter as tk
+from api import fetch_resource_names, ResourceType
 
 class ui:
     def __init__(self, master=None):
+        self.scarab_values = fetch_resource_names(ResourceType.SCARAB)
+        self.compass_values = fetch_resource_names(ResourceType.COMPASS)
         self.mapset_frame = ttk.Frame(master, name="mapset_frame")
         self.mapset_frame.configure(height=200, width=200)
         
         self.label_scarab_one = ttk.Label(self.mapset_frame, name="label_scarab_one")
         self.label_scarab_one.configure(text='Scarab 1')
         self.label_scarab_one.grid(column=0, padx=10, row=0)
-        self.entry_scarab_one = ttk.Entry(self.mapset_frame, name="entry_scarab_one")
-        self.entry_scarab_one.grid(column=1, padx=10, pady=10, row=0)
+        self.choice_scarab_one = tk.StringVar(self.mapset_frame)
+        self.choice_scarab_one.set("Scarab 1 Option")     
+        self.dropdown_scarab_one = ttk.OptionMenu(self.mapset_frame, self.choice_scarab_one, *self.scarab_values)
+        self.dropdown_scarab_one.grid(column=1, padx=10, pady=10, row=0)
         
         self.label_scarab_two = ttk.Label(self.mapset_frame, name="label_scarab_two")
         self.label_scarab_two.configure(text='Scarab 2')
         self.label_scarab_two.grid(column=0, row=1)
-        self.entry_scarab_two = ttk.Entry(self.mapset_frame, name="entry_scarab_two")
-        self.entry_scarab_two.grid(column=1, padx=10, pady=10, row=1)
+        self.choice_scarab_two = tk.StringVar(self.mapset_frame)
+        self.choice_scarab_two.set("Scarab 2 Option")
+        self.dropdown_scarab_two = ttk.OptionMenu(self.mapset_frame, self.choice_scarab_two,  *self.scarab_values)
+        self.dropdown_scarab_two.grid(column=1, padx=10, pady=10, row=1)
         
         self.label_scarab_three = ttk.Label(self.mapset_frame, name="label_scarab_three")
         self.label_scarab_three.configure(text='Scarab 3')
         self.label_scarab_three.grid(column=0, row=2)
-        self.entry_scarab_three = ttk.Entry(self.mapset_frame, name="entry_scarab_three")
-        self.entry_scarab_three.grid(column=1, padx=10, pady=10, row=2)
+        self.choice_scarab_three = tk.StringVar(self.mapset_frame)
+        self.choice_scarab_three.set("Scarab 3 Option")
+        self.dropdown_scarab_three = ttk.OptionMenu(self.mapset_frame, self.choice_scarab_three, *self.scarab_values)
+        self.dropdown_scarab_three.grid(column=1, padx=10, pady=10, row=2)
         
         self.label_scarab_four = ttk.Label(self.mapset_frame, name="label_scarab_four")
         self.label_scarab_four.configure(text='Scarab 4')
         self.label_scarab_four.grid(column=0, row=3)
-        self.entry_scarab_four = ttk.Entry(self.mapset_frame, name="entry_scarab_four")
-        self.entry_scarab_four.grid(column=1, padx=10, pady=10, row=3)
+        self.choice_scarab_four = tk.StringVar(self.mapset_frame)
+        self.choice_scarab_four.set("Scarab 4 Option")
+        self.dropdown_scarab_four = ttk.OptionMenu(self.mapset_frame, self.choice_scarab_four,  *self.scarab_values)
+        self.dropdown_scarab_four.grid(column=1, padx=10, pady=10, row=3)
         
         self.label_compass_one = ttk.Label(self.mapset_frame, name="label_compass_one")
         self.label_compass_one.configure(text='Compass 1')
         self.label_compass_one.grid(column=2, padx=10, pady=10, row=0)
-        self.entry_compass_one = ttk.Entry(self.mapset_frame, name="entry_compass_one")
-        self.entry_compass_one.grid(column=3, padx=10, pady=10, row=0)
+        self.choice_compass_one = tk.StringVar(self.mapset_frame)
+        self.choice_compass_one.set("Compass 1 Option")
+        self.dropdown_compass_one = ttk.OptionMenu(self.mapset_frame, self.choice_compass_one, *self.compass_values)
+        self.dropdown_compass_one.grid(column=3, padx=10, pady=10, row=0)
         
         self.label_compass_two = ttk.Label(self.mapset_frame, name="label_compass_two")
         self.label_compass_two.configure(text='Compass 2')
         self.label_compass_two.grid(column=2, row=1)
-        self.entry_compass_two = ttk.Entry(self.mapset_frame, name="entry_compass_two")
-        self.entry_compass_two.grid(column=3, row=1)
+        self.choice_compass_two = tk.StringVar(self.mapset_frame)
+        self.choice_compass_two.set("Compass 2 Option")
+        self.dropdown_compass_two = ttk.OptionMenu(self.mapset_frame, self.choice_compass_two, *self.compass_values)
+        self.dropdown_compass_two.grid(column=3, padx=10, pady=10, row=1)
         
         self.label_compass_three = ttk.Label(self.mapset_frame, name="label_compass_three")
         self.label_compass_three.configure(text='Compass 3')
         self.label_compass_three.grid(column=2, row=2)
-        self.entry_compass_three = ttk.Entry(self.mapset_frame, name="entry_compass_three")
-        self.entry_compass_three.grid(column=3, row=2)
+        self.choice_compass_three = tk.StringVar(self.mapset_frame)
+        self.choice_compass_three.set("Compass 3 Option")
+        self.dropdown_compass_three = ttk.OptionMenu(self.mapset_frame, self.choice_compass_three, *self.compass_values)
+        self.dropdown_compass_three.grid(column=3, padx=10, pady=10, row=2)
         
         self.label_compass_four = ttk.Label(self.mapset_frame, name="label_compass_four")
         self.label_compass_four.configure(text='Compass 4')
         self.label_compass_four.grid(column=2, row=3)
-        self.entry_compass_four = ttk.Entry(self.mapset_frame, name="entry_compass_four")
-        self.entry_compass_four.grid(column=3, row=3)
+        self.choice_compass_four = tk.StringVar(self.mapset_frame)
+        self.choice_compass_four.set("Compass 4 Option")
+        self.dropdown_compass_four = ttk.OptionMenu(self.mapset_frame, self.choice_compass_four, *self.compass_values)
+        self.dropdown_compass_four.grid(column=3, padx=10, pady=10, row=3)
         
         self.label_total_cost = ttk.Label(self.mapset_frame, name="label_total_cost")
         self.label_total_cost.configure(text='Total Cost')
@@ -63,9 +83,26 @@ class ui:
         self.label_divines = ttk.Label(self.mapset_frame, name="label_divines")
         self.label_divines.grid(column=3, row=4)
         
+        self.submit = tk.Button(self.mapset_frame, text='Submit', command=self.calculate_total) 
+        self.submit.grid(column=3, row=5)
+        
         self.mapset_frame.pack(side="top")
 
         self.mainwindow = self.mapset_frame
-
+        
     def run(self):
-        self.mainwindow.mainloop()
+        self.mainwindow.mainloop()     
+        
+    def calculate_total(self):
+        total_chaos = self.scarab_values[self.choice_scarab_one.get()] \
+        + self.scarab_values[self.choice_scarab_two.get()] \
+        + self.scarab_values[self.choice_scarab_three.get()] \
+        + self.scarab_values[self.choice_scarab_four.get()] \
+        + self.compass_values[self.choice_compass_one.get()] \
+        + self.compass_values[self.choice_compass_two.get()] \
+        + self.compass_values[self.choice_compass_three.get()] \
+        + self.compass_values[self.choice_compass_four.get()]
+        
+        self.label_chaos['text'] = str(total_chaos) + "c"
+        self.label_divines['text'] = str(round(total_chaos / 215, 2)) + "div"
+        
